930b2dd5c3f8b1e61d2349cdf216b74ad15453eb
Svenstar74 <sven.firmbach@gmx.de>
2023-03-15 11:00:36 +0100

    Refactor

--- src/app/components/ContextMenu/ContextMenu.tsx
+++ src/app/components/ContextMenu/ContextMenu.tsx
@@ -1,33 +1,42 @@
+import { FC, useState } from "react";
+
 import { Paper, MenuList, ListItemIcon, ListItemText, MenuItem } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
 import NorthEastIcon from '@mui/icons-material/NorthEast';
 
 import { useApiClient } from '../../hooks/useApiClient';
-import { useAppDispatch } from "../../store/redux/hooks";
-import { showDelEdgeDialog, showNewEdgeDialog, showNewNodeDialog } from '../../store/redux/uiSlice';
 
-type ContextMenuProps = {
+import { NewNodeDialog } from "../NewNodeDialog";
+import { NewEdgeDialog } from "../NewEdgeDialog";
+import { DelEdgeDialog } from "../DelEdgeDialog";
+
+type Props = {
   show: boolean;
   position: { x: number, y: number };
   menuItems: string[];
   clickedItemId?: string;
 };
 
-export const ContextMenu = ({ show, position, menuItems, clickedItemId = '' }: ContextMenuProps) => {
+export const ContextMenu: FC<Props> = ({ show, position, menuItems, clickedItemId = '' }) => {
   const apiClient = useApiClient();
-  const dispatch = useAppDispatch();
   
-  if (!show) {
-    return null;
-  }
-    
+  const [showNewNodeDialog, setShowNewNodeDialog] = useState(false);
+  const [showNewEdgeDialog, setShowNewEdgeDialog] = useState(false);
+  const [showDelEdgeDialog, setShowDelEdgeDialog] = useState(false);
+      
   return (
     <div>
-      <Paper style={{ position: 'absolute', left: position.x, top: position.y, zIndex: '100'}}>
+      {/* Show dialog when associated menu item was clicked */}
+      <NewNodeDialog open={showNewNodeDialog} onClose={() => setShowNewNodeDialog(false)} />
+      <NewEdgeDialog open={showNewEdgeDialog} onClose={() => setShowNewEdgeDialog(false)} />
+      <DelEdgeDialog open={showDelEdgeDialog} onClose={() => setShowDelEdgeDialog(false)} />
+
+      {/* Show the dialog itself */}
+      {show && <Paper style={{ position: 'absolute', left: position.x, top: position.y, zIndex: '100'}}>
         <MenuList>       
 
-          {menuItems.includes('addNode') && <MenuItem onClick={() => dispatch(showNewNodeDialog())}>
+          {menuItems.includes('addNode') && <MenuItem onClick={() => setShowNewNodeDialog(true)}>
             <ListItemIcon>
               <AddIcon />
             </ListItemIcon>
@@ -41,14 +50,14 @@ export const ContextMenu = ({ show, position, menuItems, clickedItemId = '' }: C
             <ListItemText>Delete Node</ListItemText>
           </MenuItem>}
 
-          {menuItems.includes('addEdge') && <MenuItem onClick={() => dispatch(showNewEdgeDialog())}>
+          {menuItems.includes('addEdge') && <MenuItem onClick={() => setShowNewEdgeDialog(true)}>
             <ListItemIcon>
               <NorthEastIcon />
             </ListItemIcon>
             <ListItemText>Add Edge</ListItemText>
           </MenuItem>}
 
-          {menuItems.includes('deleteEdge') && <MenuItem onClick={() => dispatch(showDelEdgeDialog())}>
+          {menuItems.includes('deleteEdge') && <MenuItem onClick={() => setShowDelEdgeDialog(true)}>
             <ListItemIcon>
               <DeleteIcon />
             </ListItemIcon>
@@ -56,7 +65,7 @@ export const ContextMenu = ({ show, position, menuItems, clickedItemId = '' }: C
           </MenuItem>}
 
         </MenuList>
-      </Paper>
+      </Paper>}
     </div>
   );
 }
