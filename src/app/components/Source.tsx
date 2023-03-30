7e737a2e35865a59bd0875513ad4ca53f862ef5f
Sven Firmbach <sven.firmbach@gmx.de>
2023-03-30 13:25:40 +0200

    Create confirm dialog (#54)

--- src/app/components/Source.tsx
+++ src/app/components/Source.tsx
@@ -1,7 +1,8 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { IconButton, TextField, Tooltip } from '@mui/material';
 import LaunchIcon from '@mui/icons-material/Launch';
 import DeleteIcon from '@mui/icons-material/Delete';
+import ConfirmDialog from './Dialogs/ConfirmDialog';
 
 interface Props {
   climateConceptId: string;
@@ -20,54 +21,72 @@ export const Source: FC<Props> = ({
   originalText,
   onDeleteSource,
 }) => {
-  const openLink = () => {
-    window.open(url, '_blank', 'noreferrer');
-  };
+  const [showConfirmDialog, setShowConfirmDialog] = useState(false);
 
+  function confirmDeleteSource() {
+    setShowConfirmDialog(false);
+    onDeleteSource(climateConceptId, url, originalText)
+  }
+  
   return (
-    <div
-      style={{
-        marginBottom: '30px',
-        padding: '20px 10px',
-        paddingBottom: '50px',
-        border: '1px solid lightgray',
-        borderRadius: '5px',
-      }}
-    >
-      <TextField
-        fullWidth
-        variant="outlined"
-        label="Link"
-        value={url}
-        style={{ verticalAlign: 'middle', marginBottom: '20px' }}
-        InputProps={{
-          readOnly: true,
-          endAdornment: (
-            <IconButton onClick={openLink} style={{ marginLeft: '20px' }}>
-              <LaunchIcon />
-            </IconButton>
-          ),
+    <>
+      <div
+        style={{
+          marginBottom: '30px',
+          padding: '20px 10px',
+          paddingBottom: '50px',
+          border: '1px solid lightgray',
+          borderRadius: '5px',
         }}
-      />
+      >
+        <TextField
+          fullWidth
+          variant="outlined"
+          label="Link"
+          value={url}
+          style={{ verticalAlign: 'middle', marginBottom: '20px' }}
+          InputProps={{
+            readOnly: true,
+            endAdornment: (
+              <IconButton onClick={() => window.open(url, '_blank', 'noreferrer')} style={{ marginLeft: '20px' }}>
+                <LaunchIcon />
+              </IconButton>
+            ),
+          }}
+        />
 
-      <TextField
-        fullWidth
-        multiline
-        variant="outlined"
-        label="Original Text"
-        value={originalText}
-        InputProps={{
-          readOnly: true,
-        }}
-      />
+        <TextField
+          fullWidth
+          multiline
+          variant="outlined"
+          label="Original Text"
+          value={originalText}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
 
-      <Tooltip title="Delete Source" style={{ float: 'right' }}>
-        <IconButton>
-          <DeleteIcon
-            onClick={() => onDeleteSource(climateConceptId, url, originalText)}
-          />
-        </IconButton>
-      </Tooltip>
-    </div>
+        <Tooltip title="Delete Source" style={{ float: 'right' }}>
+          <IconButton>
+            <DeleteIcon
+              onClick={() => setShowConfirmDialog(true)}
+            />
+          </IconButton>
+        </Tooltip>
+      </div>
+
+      <ConfirmDialog
+        open={showConfirmDialog}
+        onCancel={() => setShowConfirmDialog(false)}
+        onConfirm={confirmDeleteSource}
+        content={
+          <>
+            <div>Are you sure you want to delete this source?</div>
+            <div>This action cannot be undone.</div>
+          </>
+        }
+        confirmBtnText="Yes"
+      />
+    </>
   );
 };
