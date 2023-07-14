1592a2dbf213e3e5c35ca6fc18bd40f241483311
Svenstar74 <sven.firmbach@gmx.de>
2023-07-14 11:05:18 +0200

    cleanup code

--- src/components/features/ToggleViewOfNodes/ToggleViewOfNodes.tsx
+++ src/components/features/ToggleViewOfNodes/ToggleViewOfNodes.tsx
@@ -10,23 +10,23 @@ import { setGroupedView } from '../../../store/redux/slices/graphSlice';
 
 function ToggleViewOfNodes() {
   const dispatch = useAppDispatch();
-  
+
   const sigma = useSigma();
   const [showGroupedNodes, setShowGroupedNodes] = useState(false);
 
   async function showAllSummaryNodes() {
-    const connections = sigma.getGraph().edges().map(edgeId => {
+    const connections = sigma.getGraph().edges().map((edgeId) => {
       return {
         id: edgeId,
         sourceId: sigma.getGraph().source(edgeId),
         targetId: sigma.getGraph().target(edgeId),
-        type: sigma.getGraph().getEdgeAttribute(edgeId, 'connectionType')
+        type: sigma.getGraph().getEdgeAttribute(edgeId, 'connectionType'),
       };
     });
 
-    const groupedNodesWithConnections = await ConnectionUseCases.getAllSummaryNodesWithConnections(connections);
+    const groupedNodesWithConnections = ConnectionUseCases.getAllSummaryNodesWithConnections(connections);
 
-    groupedNodesWithConnections.groupedNodes.forEach(group => {
+    groupedNodesWithConnections.groupedNodes.forEach((group) => {
       const id = group[0].id;
       const attributes = sigma.getGraph().getNodeAttributes(id);
 
@@ -44,43 +44,31 @@ function ToggleViewOfNodes() {
       }
     });
   }
-  
+
   async function switchView() {
     dispatch(setGroupedView(!showGroupedNodes));
-    
-    const connections = sigma.getGraph().edges().map(edgeId => {
+
+    const connections = sigma.getGraph().edges().map((edgeId) => {
       return {
         id: edgeId,
         sourceId: sigma.getGraph().source(edgeId),
         targetId: sigma.getGraph().target(edgeId),
-        type: sigma.getGraph().getEdgeAttribute(edgeId, 'connectionType')
+        type: sigma.getGraph().getEdgeAttribute(edgeId, 'connectionType'),
       };
     });
-  
+
     const groupedNodesWithConnections = ConnectionUseCases.getAllSummaryNodesWithConnections(connections);
-    
+
     if (showGroupedNodes) {
       // Switch from grouped to individual
-  
+
       // Show all individual nodes
       sigma.getGraph().nodes().forEach((node) => {
-        sigma.getGraph().setNodeAttribute(node, 'hidden', false);
-      });
-      
-      // Redirect Connections
-      // groupedNodesWithConnections.connections.forEach(connections => {
-      //   connections.forEach(connection => {
-      //     if (connection.type === 'isEqualTo') return;
-  
-      //     const attributes = sigma.getGraph().getEdgeAttributes(connection.id);
-          
-      //     sigma.getGraph().dropEdge(connection.id);
-      //     sigma.getGraph().addEdgeWithKey(connection.id, connection.sourceId, connection.targetId, { ...attributes });
-      //   });
-      // });
-  
+          sigma.getGraph().setNodeAttribute(node, 'hidden', false);
+        });
+
       // Delete grouped nodes
-      sigma.getGraph().nodes().forEach(node => {
+      sigma.getGraph().nodes().forEach((node) => {
         const size = sigma.getGraph().getNodeAttribute(node, 'size');
         if (size === 5) {
           sigma.getGraph().dropNode(node);
@@ -88,91 +76,67 @@ function ToggleViewOfNodes() {
       });
     } else {
       // Switch from individual to grouped
-      
+
       // Hide all individual nodes that are part of a group
-      groupedNodesWithConnections.groupedNodes.forEach(group => {
-        group.forEach(node => {
+      groupedNodesWithConnections.groupedNodes.forEach((group) => {
+        group.forEach((node) => {
           sigma.getGraph().setNodeAttribute(node.id, 'hidden', true);
         });
       });
-  
+
       // Show summary nodes
-      await showAllSummaryNodes();  
-  
+      await showAllSummaryNodes();
+
       // Add Connections
       groupedNodesWithConnections.connections.forEach((connections, index) => {
-        connections.forEach(connection => {
-          const isSourcePartOfGroup = groupedNodesWithConnections.groupedNodes[index].some(node => node.id === connection.sourceId);
-          const isTargetPartOfGroup = groupedNodesWithConnections.groupedNodes[index].some(node => node.id === connection.targetId);
+        connections.forEach((connection) => {
+          const isSourcePartOfGroup = groupedNodesWithConnections.groupedNodes[index].some(
+            (node) => node.id === connection.sourceId
+          );
+          const isTargetPartOfGroup = groupedNodesWithConnections.groupedNodes[index].some(
+            (node) => node.id === connection.targetId
+          );
           if (isSourcePartOfGroup) {
             const attributes = sigma.getGraph().getEdgeAttributes(connection.id);
             const label = sigma.getGraph().getNodeAttribute(groupedNodesWithConnections.groupedNodes[index][0].id, 'label');
 
-            const isTargetPartOfAnyGroup = groupedNodesWithConnections.groupedNodes.findIndex(group => group.some(node => node.id === connection.targetId));;
+            const isTargetPartOfAnyGroup = groupedNodesWithConnections.groupedNodes.findIndex(
+              (group) => group.some((node) => node.id === connection.targetId)
+            );
             if (isTargetPartOfAnyGroup >= 0) {
               const targetLabel = sigma.getGraph().getNodeAttribute(groupedNodesWithConnections.groupedNodes[isTargetPartOfAnyGroup][0].id, 'label');
-              try { sigma.getGraph().addEdgeWithKey(connection.id + " ", label, targetLabel, { ...attributes }); } catch {}
+              try {
+                sigma.getGraph().addEdgeWithKey(connection.id + ' ', label, targetLabel, { ...attributes });
+              } catch {}
             }
-            
-            try { sigma.getGraph().addEdgeWithKey(connection.id + " ", label, connection.targetId, { ...attributes }); } catch {}
+
+            try {
+              sigma.getGraph().addEdgeWithKey(connection.id + ' ', label, connection.targetId, { ...attributes });
+            } catch {}
           } else if (isTargetPartOfGroup) {
             const attributes = sigma.getGraph().getEdgeAttributes(connection.id);
             const label = sigma.getGraph().getNodeAttribute(groupedNodesWithConnections.groupedNodes[index][0].id, 'label');
 
-            const isSourcePartOfAnyGroup = groupedNodesWithConnections.groupedNodes.findIndex(group => group.some(node => node.id === connection.sourceId));;
+            const isSourcePartOfAnyGroup = groupedNodesWithConnections.groupedNodes.findIndex(
+              (group) => group.some((node) => node.id === connection.sourceId)
+            );
             if (isSourcePartOfAnyGroup >= 0) {
               const sourceLabel = sigma.getGraph().getNodeAttribute(groupedNodesWithConnections.groupedNodes[isSourcePartOfAnyGroup][0].id, 'label');
-              try { sigma.getGraph().addEdgeWithKey(connection.id + " ", sourceLabel, label, { ...attributes }); } catch {}
+              try {
+                sigma.getGraph().addEdgeWithKey(connection.id + ' ', sourceLabel, label, { ...attributes });
+              } catch {}
             }
-            
-            try { sigma.getGraph().addEdgeWithKey(connection.id + " ", connection.sourceId, label, { ...attributes }); } catch {}
+
+            try {
+              sigma.getGraph().addEdgeWithKey(connection.id + ' ', connection.sourceId, label, { ...attributes });
+            } catch {}
           }
         });
       });
-      
-      // Redirect connections
-      // groupedNodesWithConnections.connections.forEach(connections => {
-      //   connections.forEach(connection => {    
-      //     if (connection.type === 'isEqualTo') return;
-          
-      //     const sourceGroupIndex = groupedNodesWithConnections.groupedNodes.findIndex(group => group.some(node => node.id === connection.sourceId));
-      //     const targetGroupIndex = groupedNodesWithConnections.groupedNodes.findIndex(group => group.some(node => node.id === connection.targetId));
-  
-      //     const sourceLabel = sourceGroupIndex >= 0 ? sigma.getGraph().getNodeAttribute(groupedNodesWithConnections.groupedNodes[sourceGroupIndex][0].id, 'label') : null;
-      //     const targetLabel = targetGroupIndex >= 0 ? sigma.getGraph().getNodeAttribute(groupedNodesWithConnections.groupedNodes[targetGroupIndex][0].id, 'label') : null;
-  
-      //     if (sourceLabel === targetLabel) return;
-  
-      //     if (sourceGroupIndex !== -1 && targetGroupIndex !== -1) {
-      //       const attributes = sigma.getGraph().getEdgeAttributes(connection.id);
-  
-      //       sigma.getGraph().dropEdge(connection.id);
-      //       try {
-      //         sigma.getGraph().addEdgeWithKey(connection.id, sourceLabel, targetLabel, { ...attributes });
-      //       } catch {}
-      //     } else if (sourceGroupIndex !== -1) {
-      //       const attributes = sigma.getGraph().getEdgeAttributes(connection.id);
-  
-      //       sigma.getGraph().dropEdge(connection.id);
-      //       try {
-      //         sigma.getGraph().addEdgeWithKey(connection.id, sourceLabel, connection.targetId, { ...attributes });
-      //       } catch {}
-      //     } else if (targetGroupIndex !== -1) {
-      //       const attributes = sigma.getGraph().getEdgeAttributes(connection.id);
-  
-      //       sigma.getGraph().dropEdge(connection.id);
-      //       try {
-      //         sigma.getGraph().addEdgeWithKey(connection.id, connection.sourceId, targetLabel, { ...attributes });
-      //       } catch {}
-      //     }
-      //   });
-      // });
     }
-  
-    setShowGroupedNodes(prevState => !prevState);
+
+    setShowGroupedNodes((prevState) => !prevState);
   }
-  
-  
 
   return (
     <div style={{ position: 'absolute', right: 60, top: 10 }}>
